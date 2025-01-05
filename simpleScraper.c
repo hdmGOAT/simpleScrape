997c192309ac53ac27734624e1d045025a6b6f8f
Hans Matthew Del Mundo <hdmGOAT@users.noreply.github.com>
2025-01-05 16:19:53 +0800

    progress

--- simpleScraper.c
+++ simpleScraper.c
@@ -1,4 +1,23 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
-int main()
+int main(int argc, char *argv){
+    CURL *curl;
+    CURLcode result;
+
+    curl = curl_easy_init;
+    if (curl == NULL){
+        fprintf(stderr, "HTTP request failed\n");
+        return -1;
+    }
+
+    curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.com");
+
+    result = curl_easy_perform(curl);
+    if(result != CURLE_OK){
+        fprintf(stderr, "Error: %s\n", curl_easy_strerror(result));
+        return -1;
+    }
+    curl_easy_cleanup(curl);
+    return 0;
+}
